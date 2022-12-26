1368b3c1d3b5ee00e03dcfcf49bf99c95fa98dde
dupolo <danilocbjp@hotmail.com>
2022-12-25 22:40:44 -0300

    new file:   Apostas CSGO/__pycache__/odds.cpython-311.pyc new file:   Apostas CSGO/__pycache__/saldos.cpython-311.pyc new file:   Apostas CSGO/saldos.py new file:   Apostas CSGO/saldos.txt new file:   Apostas CSGO/script.py

---
+++ Apostas CSGO/saldos.py
@@ -0,0 +1,55 @@
+import shutil, tempfile
+
+def saldo():
+    for i in range(3):
+        print()
+    for i in open('saldos.txt', 'r'):
+        print(f"Saldo de {i}", end="")
+    for i in range(2):
+        print()
+
+def apostar(nome, aposta):
+    with open('saldos.txt', 'r') as arquivo, \
+        tempfile.NamedTemporaryFile('w', delete=False) as out:
+        for linha in arquivo:
+            if nome in linha[:18]:
+                codigo = float(linha[18:22])
+                if aposta > codigo:
+                    return True
+    return False
+
+def retirar(nome, aposta):
+    with open('saldos.txt', 'r') as arquivo, \
+            tempfile.NamedTemporaryFile('w', delete=False) as out:
+            for linha in arquivo:
+                if nome in linha[:18]:
+                    codigo = float(linha[18:22])
+                    codigo -= aposta
+                    linha = linha[:18] + str(f"{codigo:.2f}") + " coins\n"
+                out.write(linha)
+    shutil.move(out.name, 'saldos.txt')
+    
+def adicionar(nome, valor):
+    with open('saldos.txt', 'r') as arquivo, \
+            tempfile.NamedTemporaryFile('w', delete=False) as out:
+            for linha in arquivo:
+                if nome in linha[:18]:
+                    codigo = float(linha[18:22])
+                    codigo += valor
+                    linha = linha[:18] + str(f"{codigo:.2f}") + " coins\n"
+                out.write(linha)
+    shutil.move(out.name, 'saldos.txt')
+
+def zerou(nome):
+    with open('saldos.txt', 'r') as arquivo, \
+            tempfile.NamedTemporaryFile('w', delete=False) as out:
+            for linha in arquivo:
+                if nome in linha[:18]:
+                    codigo = float(linha[18:22])
+                    if codigo == 0:
+                        linha = linha[:18] + str(00.25) + " coins\n"
+                        print(f"O jogador {nome} zerou o saldo, logo fica com 0.25 coins.")
+                    else:
+                        print(f"O jogador {nome} não zerou.")
+                out.write(linha)
+    shutil.move(out.name, 'saldos.txt')
---
+++ Apostas CSGO/script.py
@@ -0,0 +1,89 @@
+import saldos, time
+
+porcentagem = {
+    "Arthur" : [1.50, 1.70],
+    "Danilo" : [2.20, 1.30],
+    "Gabriel franca" : [1.50, 1.40],
+    "Gabriel souza" : [1.50, 1.50],
+    "Gustavo" : [1.77, 1.85],
+    "Jubao" : [2.00, 1.40],
+    "Rubens" : [1.60, 1.45],
+    "Paulo" : [1.55, 1.35]
+}
+lista_escolha = []
+lista_jogador = []
+lista_apostas = []
+lista_nome = []
+iniciou = False
+
+
+while True:
+    saldos.saldo()
+    oper = int(input("Diga a operação:\n0-Encerra o programa\n1-Iniciar apostas\n2-Finalizar apostas\n"))
+    if oper == 0:
+        break
+    elif oper == 1:
+        if not iniciou:
+            iniciou = True
+            qtd = int(input("Diga quantos participantes jogarão: "))
+            for i in range(qtd):
+                jogador = input(f"Diga o nome do participante-{i+1}: ").capitalize()
+                while jogador not in porcentagem.keys():
+                    print("O nome não está na lista de jogadores! Diga um jogador válido.")
+                    jogador = input(f"Diga o nome do participante-{i+1}: ").capitalize()
+                while jogador in lista_jogador:
+                    print("Esse jogador já foi escolhido!")
+                    jogador = input(f"Diga o nome do participante-{i+1}: ").capitalize()
+                while True:
+                    try:
+                        aposta = float(input("Diga a sua aposta: "))
+                        break
+                    except Exception:
+                        print("Diga um valor válido!")
+                while saldos.apostar(jogador, aposta): # Verifica o saldo do jogador
+                    while True:
+                        try:
+                            aposta = float(input("Você não tem esse dinheiro para apostar!\nDiga uma aposta válida: "))
+                            break
+                        except Exception:
+                            print("Diga um valor válido!")
+                nome = input("Em quem você vai apostar: ").capitalize()
+                while nome == jogador or nome not in porcentagem.keys():
+                    print("Diga um nome válido!")
+                    nome = input("Em quem você vai apostar: ").capitalize()
+                while True:
+                    try:
+                        escolha = int(input("1-Over --- 2-Under\n"))
+                        break
+                    except Exception:
+                        print("Diga um valor válido!")
+                saldos.retirar(jogador, aposta) # Retira o valor da aposta do saldo
+                lista_jogador.append(jogador)
+                lista_apostas.append(aposta)
+                lista_escolha.append(escolha)
+                lista_nome.append(nome)
+        else:
+            print("Você já iniciou um jogo!")
+            time.sleep(1)
+    elif oper == 2:
+        if iniciou:
+            for i, x in enumerate(lista_jogador):
+                print(f"Participante - {x}")
+                resultado = int(input("0-Perdeu --- 1-Ganhou\n"))
+                if resultado == 1:
+                    if lista_escolha[i] == 1: # Apostou Over
+                        valor = lista_apostas[i] * porcentagem[lista_nome[i]][0]
+                        saldos.adicionar(x, valor)
+                    else: # Apostou Under
+                        valor = lista_apostas[i] * porcentagem[lista_nome[i]][1]
+                        saldos.adicionar(x, valor)
+                saldos.zerou(x)
+            lista_jogador.clear()
+            lista_apostas.clear()
+            lista_escolha.clear()
+            lista_nome.clear()
+            iniciou = False
+            time.sleep(1)
+        else:
+            print("Nenhum jogo começou!")
+            time.sleep(1)
